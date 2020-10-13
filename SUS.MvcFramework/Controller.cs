82900e6dd38a1d0875e3a3fa9ea2dcb1c7416a79
Stoyan Boyadzhiev <stoyan.s.boyadzhiev@gmail.com>
2020-10-13 19:41:44 +0300

    Basic SUSViewEngine done (lesson)! Created by Nikolay.IT

--- SUS.MvcFramework/Controller.cs
+++ SUS.MvcFramework/Controller.cs
@@ -1,4 +1,5 @@
 ﻿using SUS.HTTP;
+using SUS.MvcFramework.ViewEngine;
 
 using System;
 using System.Runtime.CompilerServices;
@@ -8,17 +9,27 @@ namespace SUS.MvcFramework
 {
     public abstract class Controller
     {
-        public HttpResponse View([CallerMemberName]string path = null)
+        private SUSViewEngine viewEngine;
+
+        public Controller()
+        {
+            this.viewEngine = new SUSViewEngine();
+        }
+
+        public HttpResponse View(object viewModel = null,
+            [CallerMemberName]string path = null)
         {
             var layout = System.IO.File.ReadAllText("Views/Shared/_Layout.cshtml");
-             
+            layout = layout.Replace("@RenderBody()", "VIEW_GOES_HERE");
+            layout = this.viewEngine.GetHtml(layout, viewModel);
 
             var viewContent = System.IO.File.ReadAllText(
                 "Views/"+ 
                 this.GetType().Name.Replace("Controller", String.Empty) + 
                 "/" + path + ".cshtml");
+            viewContent = this.viewEngine.GetHtml(viewContent, viewModel);
 
-            var responseHtml = layout.Replace("@RenderBody()", viewContent);
+            var responseHtml = layout.Replace("VIEW_GOES_HERE", viewContent);
 
             var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
             var response = new HttpResponse("text.html", responseBodyBytes);
